08662d046fb1b61e713d8a90d364b743c6907d6f
Pranav Maneriker <pranavmane@gmail.com>
2023-06-26 09:16:18 +0530

    4 slides

--- avoir-video/main_script.py
+++ avoir-video/main_script.py
@@ -2,29 +2,40 @@ from manim import *
 import numpy as np
 import math
 
+
+## The scene setup can be an annotation
+
 def title_slide(s: Scene, section_name="title"):
     s.next_section(section_name)
-    title_text = "\section*{Online Fairness Auditing \\\\ through Iterative Refinement}"
+    #img = ImageMobject("images/OSU-logo.png")
+    img = SVGMobject("images/OSU-logo.svg")
+    #img.scale(3)
+    bg_rect = BackgroundRectangle(img, color=WHITE)
+    title_text = r"\section*{Online Fairness Auditing \\ through Iterative Refinement}"
     text1 = Tex(title_text, height=1.5)
     text1.shift(UP)
     s.add(text1)
-    text_emails = Tex('''\\{maneriker.1, burley.22, parthasarathy.2\\} \\\\
+    text_emails = Tex(r'''\{maneriker.1, burley.22, parthasarathy.2\} \\
                       @osu.edu''')
     text_emails.shift(DOWN)
     s.add(text_emails)
     text_conf = Tex("KDD 2023")
     text_conf.shift(2*DOWN)
     s.add(text_conf)
+    Group(img, bg_rect).next_to(text_conf, DOWN + LEFT)
+    s.add(bg_rect)
+    s.add(img)
     s.wait(duration=5*DEFAULT_WAIT_TIME)
 
 
 def avoir_goal(s: Scene, section_name="avoir_goal"):
+    #s.next_section(section_name, skip_animations=True)
     s.next_section(section_name)
 
-    text2 = Tex('''\\textbf{A}uditing and \\textbf{V}erifying fairness \\\\ \\textbf{O}nline through \\textbf{I}terative \\textbf{R}efinement''')
+    text2 = Tex(r'''\textbf{A}uditing and \textbf{V}erifying fairness \\ \textbf{O}nline through \textbf{I}terative \textbf{R}efinement''')
     text2.shift(2*UP)
-    s.play(AddTextWordByWord(text2))
-    text3 = Tex('''\\textbf{AVOIR}''')
+    s.play(AddTextWordByWord(text2), run_time=4)
+    text3 = Tex(r'''\textbf{AVOIR}''')
     text3.shift(2*UP)
     s.play(Transform(text2, text3))
 
@@ -45,15 +56,82 @@ def avoir_goal(s: Scene, section_name="avoir_goal"):
     s.play(FadeIn(axes))
     func = axes.plot(lambda x: 0.5 * np.sin(x) + 1, color=BLUE, x_range=[1, 9])
 
-    seesaw_line = Line(start= DOWN, end=DOWN)
-    seesaw_line.next_to(svg_2, DOWN)
-    s.add(seesaw_line) 
+    seesaw_line = Line(start=2*LEFT, end=2*RIGHT)
+    male_svg = SVGMobject("images/2/male.svg", height=0.8)
+    female_svg = SVGMobject("images/2/female.svg", height=0.8)
+    male_svg.next_to(seesaw_line.start, UP)
+    female_svg.next_to(seesaw_line.end, UP)
+    seesaw_except_base = Group(seesaw_line, male_svg, female_svg)
+    tri_base = Triangle()
+    tri_base.next_to(seesaw_line, DOWN)
+    seesaw = Group(seesaw_except_base, tri_base)
+    seesaw.next_to(svg_2, 5*DOWN)
+    s.add(seesaw)
     
     # three part animation
-    s.play(Create(func))
+    anim_seesaw = Succession(Rotate(seesaw_except_base, angle=PI/8, run_time=1.5),
+                             Rotate(seesaw_except_base, angle=-PI/4, run_time=2),
+                             lag_ratio=1)
+    s.play(anim_seesaw, Create(func, run_time=4.5))
 
     s.wait()
 
+def avoir_probabilstic_est(s: Scene, scene_name="avoir_challenge"):
+    s.next_section(scene_name)
+    tex_preamble = TexTemplate()
+    tex_preamble.add_to_preamble('''
+    \\usepackage{amsmath,amssymb}
+    ''')
+    head_text = Tex(r"\section*{AVOIR Probabilstic estimates}")
+    head_text.shift(2* (UP))
+    s.add(head_text)
+    spec = MathTex(r'''
+        \text{Specification } \psi
+    ''', tex_template=tex_preamble)
+    spec.shift(UP)
+    s.play(Create(spec))
+    s.wait()
+    anytime_true = MathTex(r'P[', r'\forall t > 1', r', \psi] \geq 1-', r'\Delta', tex_template=tex_preamble)
+
+    s.play(Create(anytime_true))
+    s.wait()
+    
+    sel_rect_2 = SurroundingRectangle(anytime_true[3], color=BLUE)
+    sel_text_2 = Tex("Threshold probability").scale(0.7)
+    sel_text_2.next_to(sel_rect_2, DOWN)
+    s.play(Create(sel_text_2), Create(sel_rect_2))
+    s.play(FadeOut(sel_text_2, sel_rect_2))
+
+    sel_rect_1 = SurroundingRectangle(anytime_true[1], color=YELLOW)
+    sel_text_1 = Tex(r"\# Observed Samples").scale(0.7)
+    sel_text_1.next_to(sel_rect_1, DOWN)
+    text_describe_t = MathTex(r'\mathbf{\neq} \text{ Apriori sample size selection}')
+    text_describe_t.next_to(sel_text_1, 2 * DOWN)
+    s.play(Create(text_describe_t))
+    s.play(Create(sel_rect_1))
+    s.play(Create(sel_text_1))
+    s.wait()
+    s.play(FadeOut(sel_text_1, sel_rect_1))
+
+    
+def implementation(s: Scene, scene_name="implementaiton"):
+    s.next_section(scene_name)
+    chart_title = Tex(r"\section*{Implementation}")
+    chart_title.shift(2*UP)
+    s.add(chart_title)
+
+    init_text = MathTex(r"\text{Random Variables } X \in \{0, 1\}")
+    init_text.shift(UP)
+    s.play(Create(init_text))
+    s.wait()
+
+    formula_text = MathTex(r'P[|', r'\bar{E}_t[X] - E[X]| \geq \varepsilon(t, \delta)] \leq \delta')
+    s.play(Create(formula_text))
+    s.wait()
+
+    figure_ax = Axes(x_range=[0, 100], y_range=[-0.2, 1.2], y_length=2, x_length=10)
+    figure_ax.next_to(formula_text, DOWN)
+    s.add(figure_ax)
 
 
 class AVOIRVideo(Scene):
@@ -61,44 +139,16 @@ class AVOIRVideo(Scene):
         # sections
 
         # slide 1:
-        #SLIDES = [0, 1]
-        SLIDES = [1]
+        SLIDES = [3]
+        #SLIDES = range(3)
         for s in SLIDES:
             self.make_slide(s)
             self.clear()
 
-        # slide 2
-
-        #rect = Rectangle()
-        #rect.set_fill(WHITE, opacity=1.0)
-        #svg_img= SVGMobject("images/2/2_1.svg", opacity=1, height=2, width=2, color=WHITE)
-        #self.add(rect)
-        #self.add(svg_img)
-
-        #self.wait()
-
     def make_slide(self, sno):
         return {
             0: title_slide,
-            1: avoir_goal
+            1: avoir_goal,
+            2: avoir_probabilstic_est,
+            3: implementation
         }[sno](self)
-
-
-   # def construct_2(self):
-   #     svg_img= SVGMobject("images/2/2_1.svg", opacity=0.5, height=2, width=2)
-   #     circle = Circle(radius=1, color=BLUE)
-   #     dot = Dot()
-   #     dot2 = dot.copy().shift(RIGHT)
-   #     self.add(dot)
-   #     svg_img.next_to(circle)
-   #     self.add(svg_img)
-
-   #     line = Line([3, 0, 0], [5, 0, 0])
-   #     self.add(line)
-
-   #     self.play(Create(svg_img))
-   #     self.play(GrowFromCenter(circle))
-   #     self.play(Transform(dot, dot2))
-   #     self.play(MoveAlongPath(dot, circle), run_time=2, rate_func=linear)
-   #     self.play(Rotating(dot, about_point=[2, 0, 0]), run_time=1.5)
-   #     self.wait()
--- script.md
+++ script.md
@@ -1,5 +1,29 @@
-Script for Talk
+AVOIR Promotional Video
 ===============
+
+## Description
+
+> Instructions: You are expected to provide a very short description of your promotional video and is limited to 1,024 characters (which includes spaces) and be an actual description of the video (something original), not the abstract of your submission.
+If your description text exceeds 1,024 characters, the description will be removed and "Promotional Video" used..
+NOTE: Authors cannot upload their videos to their own, corporate, or institution's YouTube Channels.
+
+A sizable proportion of deployed machine learning models make their decisions in a black-box manner.
+Such decision-making procedures are susceptible to intrinsic biases, which has led to a call for accountability in deployed decision systems. 
+In this video, we describe the main ideas behind AVOIR.
+AVOIR is an automated, probabilistic inference-based system for tracking the runtime fairness properties of black-box machine learning decision systems. 
+
+AVOIR provably improves upon guarantees provided by prior work and also provides a visual mechanism to audit models that violate fairness properties. AVOIR is applicable across a wide range of group fairness definitions. 
+
+***
+***
+# Slides
+
+In the remainder,
+```
+Describes accompanying animations
+```
+
+> Is the spoken text accompanying the slide
 
 ## 1. Online Fairness Auditing through Iterative Refinement
 
@@ -31,14 +55,51 @@ Seesaw with male and female
 ## 3. Challenge
 > The runtime behavior of a model may differ from what that at training time and thus, that is when we want to make estimates of its fairness behavior.
 
-> AVOIR makes probabilistic assertions, which look like
+> AVOIR needs to make probabilistic assertions, which look like
 ```
 Assertion here
 ```
-> This comes with a few challenges.
+> This can be challenging
 
-> First, we cannot apriori decide the sample size m, because we may be forced to discard collected data.
+> we cannot apriori decide the sample size m, because we may be forced to discard collected data. Peeking at values would be equivalent to p-hacking.
 
+
+*** 
+## 4. Implementation
+
+> We start with confidence sets that can make probabilistic guarantees in the online setting of the form 
+```
+Confidence statement expression and plot
+```
+
+> and these guarantees are asymptotically true. We call these terms elementary subexpressions
+
+
+> We then devise a grammar and inference rules that allow computation of guarantees are combinations of these elementary subepressions.
+
+```
+Show graph combination of two terms producing an overall confidence
+```
+***
+## 5. Challenge: Optimization
+> The next question is this - given a fixed budget/probability threshold, can we distribute it such that we can achieve this across multiple expressions while minimizing the total number of samples?
+
+```
+Animation for these samples
+```
+
+> We develop optimizaiton inference rules within AVOIR which can provably achieve guarantees in fewer steps than prior state-of-the art.
+
+
+***
+### 6. Thanks, references, credits.
+
+> Please see our paper and talk for further details and case studies showing how AVOIR can be used with real world data! 
+```
+Show example graphs from case studies
+```
+Paper link: https://doi.org/10.1145/3580305.3599454
+Code link: https://github.com/pranavmaneriker/AVOIR
 
 
 
